a7c169c595a0934ddd45828cee53f586f0a0b59f
HrithikMittal <adhikanshmittal@gmail.com>
2020-04-03 11:50:23 +0530

    Creating home

--- src/App.js
+++ src/App.js
@@ -1,50 +1,11 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-import Loading from "./Loading";
-
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: [],
-      errors: "",
-      loading: false
-    };
-  }
-
-  getUsers() {
-    this.setState({ loading: true });
-    axios
-      .get("https://api.randomuser.me/?nat=US&results=5")
-      .then(res => {
-        var exusers = this.state.users;
-        var totalusers = exusers.concat(res.data.results);
-        this.setState({ users: totalusers, loading: false });
-      })
-      .catch(err => {
-        this.setState({ errors: err.message });
-      });
-  }
-
-  componentWillMount() {
-    this.getUsers();
-  }
-
-  loadMoreUsers = () => {
-    this.getUsers();
-  };
-
   render() {
-    var displayUsers = this.state.users.map(person => {
-      return <h3 key={person.cell}>{person.name.first}</h3>;
-    });
     return (
-      <div>
-        <button onClick={this.loadMoreUsers.bind(this, "Hey")}>
-          Click Me to Load More
-        </button>
-        {this.state.loading ? <Loading /> : displayUsers}
+      <div className="container">
+        <h1>React Frontend</h1>
       </div>
     );
   }
---
+++ src/core/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="jumbotron">
+      <h2>Hello</h2>
+      <p className="lead">Welcome to React FrontEnd</p>
+    </div>
+  );
+};
+
+export default Home;
