21c7e8dd4a8c4bd2b61d4985ffe68ba0c231bc34
HrithikMittal <adhikanshmittal@gmail.com>
2020-04-03 18:21:56 +0530

    make signout

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,14 +1,69 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 
-const Menu = () => {
+const isActive = (history, path) => {
+  if (history.location.pathname === path) return { color: "#FF9900" };
+  else return { color: "#FFFFFF" };
+};
+
+export const signout = next => {
+  if (typeof window != "undefined") localStorage.removeItem("jwt");
+  next();
+  return fetch("https://localhost:8080/signout", {
+    method: "GET"
+  })
+    .then(response => {
+      console.log("signout", response);
+      return response.json();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+const Menu = ({ history }) => {
   return (
     <div>
-      <Link to="/">Home</Link>
-      <Link to="/signup">Sign Up</Link>
-      <Link to="/signin">Sign In</Link>
+      <ul className="nav nav-tabs bg-primary">
+        <li className="nav-item">
+          <Link className="nav-link" style={isActive(history, "/")} to="/">
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            className="nav-link"
+            style={isActive(history, "/signin")}
+            to="/signin"
+          >
+            Sign In
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            className="nav-link"
+            style={isActive(history, "/signup")}
+            to="/signup"
+          >
+            Sign Up
+          </Link>
+        </li>
+        <li className="nav-item">
+          <a
+            className="nav-link"
+            onClick={() => signout(() => history.push("/"))}
+            style={
+              (isActive(history, "/signup"),
+              { cursor: "pointer", color: "#FFF" })
+            }
+          >
+            Sign Out
+          </a>
+        </li>
+        {/* {JSON.stringify(history)} */}
+      </ul>
     </div>
   );
 };
 
-export default Menu;
+export default withRouter(Menu);
